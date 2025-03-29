446452aa3083e11f76e430a91f360d30f62d1b02
Timmydrax <timmydrax@gmail.com>
2025-03-29 03:23:31 +0100

    FEAT: Find Medication Feature

--- Frontend/Frontend/src/pages/FindMeds.jsx
+++ Frontend/Frontend/src/pages/FindMeds.jsx
@@ -5,8 +5,7 @@ const FindMeds = () => {
   return (
     <>
       <section className="findmeds-wrapper">
-
-        
+        <h1>This is a Random text to test my Git workflow.</h1>
       </section>
     </>
   );
