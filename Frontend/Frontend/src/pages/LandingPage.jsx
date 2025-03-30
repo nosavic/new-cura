82af455dacede41b47517ddf9730ec9d7553fd32
wisdomoladipupo <wisbel71@gmail.com>
2025-03-30 19:48:49 +0100

    updated landing page

--- Frontend/Frontend/src/pages/LandingPage.jsx
+++ Frontend/Frontend/src/pages/LandingPage.jsx
@@ -42,7 +42,6 @@ const LandingPage = () => {
           <img src="./src/assets/BannerPicture.svg" alt="Banner picture" />
         </div>
       </div>
-
       <div className="second-level-content">
         <h2>Connecting Patients to Pharmacies For Easy Medication Access.</h2>
         <p className="second-level-content-p">
@@ -56,15 +55,14 @@ const LandingPage = () => {
         <div className="Benefits">
           <div>
             <img src="./src/assets/vector.svg" alt="vector svg image" />
-            <div>
-              <span>
+            <div className="benefit-text first-benefits">
+              <span className="first-benefits">
                 <h3>Speed and Accessibility</h3>
                 <p>Convey Free and Hassle Free Mediction Access</p>
               </span>
             </div>
           </div>
-
-          <div>
+          <div className="benefit-text second-benefits">
             <img src="./src/assets/padlock.svg" alt="vector svg image" />
             <span>
               <h3>Trust & Security</h3>
@@ -76,7 +74,7 @@ const LandingPage = () => {
             </span>
           </div>
 
-          <div>
+          <div className="benefit-text third-benefits">
             <span>
               <h3>Innovation And Digital Transformation</h3>
               <p>
@@ -89,15 +87,35 @@ const LandingPage = () => {
         </div>
       </div>
       <div className="third-level-content">
-        <h2>Your Health Will Thank You</h2>
+        <div className="third-level-content-text">
+          <h2>Your Health Will Thank You</h2>
+          <p>
+            order Easily, find Genuine Drug Instantly,Pay securely and Pick up
+            With Confidence-experience ,Fast safe and Reliable Access to
+            Medication With Curamap
+          </p>
+          <button>Browse Medications</button>
+        </div>
+        <img src="./src/assets/Thirdlevelimg.svg" alt="heart image" />
+      </div>
 
-        <p>
-          order Easily, find Genuine Drug Instantly,Pay securely and Pick up
-          With Confidence-experience ,Fast safe and Reliable Access to
-          Medication With Curamap
-        </p>
-
-        <button>Browse Medications</button>
+      <div className="fourth-level-content">
+        <h3>How is curamap Different ?</h3>
+        <div className="card card-1">
+          <img src="./src/assets/card-1-img.svg" alt="Tablets image" />
+          <h4>Tab Valsatan 2.5mg</h4>
+          <p>Take one Once a Day For 7 Days</p>
+          <span>
+            <p>Curamap</p>
+            <b># 3,500</b>
+          </span>
+          <span>
+            <p>Curamap</p>
+            <b># 4,500</b>
+          </span>
+        </div>
+        <div className="card card-2"></div>
+        <div className="card card-3"></div>
       </div>
     </div>
   );
