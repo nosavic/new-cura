3567b16f60700fd535840fbb4a6f23668eb86207
unknown <Tawakalt10>
2025-04-03 20:06:32 +0100

    new

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -3,16 +3,19 @@ import LandingPage from "./pages/LandingPage";
 import SignUp from "./components/forms/SignUp";
 import SignIn from "./components/forms/SignIn";
 import ProcessingPage from "./components/ProcessingPage";
+import PaymentModal from "./components/PaymentModal";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<SignUp />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path = "/processing" element = {<ProcessingPage/>}/>
+      <Route path = "/paymentmodal" element = {<PaymentModal/>}/>
+      <Route path = "/paymentmodal" element = { <PaymentModal/>}/>
     </Routes>
-  );
+  ); 
 }
 
 export default App;
