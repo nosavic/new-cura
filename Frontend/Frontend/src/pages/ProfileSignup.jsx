b8acc795e40721f15060654b607a411965a3a477
Timmydrax <timmydrax@gmail.com>
2025-04-03 13:12:29 +0100

    CHORE: Basic setup

---
+++ Frontend/Frontend/src/pages/ProfileSignup.jsx
@@ -0,0 +1,15 @@
+import "../styles/profile-signup.css";
+
+const ProfileSignup = () => {
+  return (
+    <>
+      <div className="stark">
+        <h1 className="arya">
+          This is to check if this component renders well
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default ProfileSignup;
