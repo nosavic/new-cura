f266cd479be6f54cb8cf60d1f901061c20092b03
Timmydrax <timmydrax@gmail.com>
2025-04-09 14:02:19 +0100

    FIX: Bug from general styling.

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -7,7 +7,6 @@ import FindMeds from "./pages/FindMeds";
 import TransactionSuccess from "./pages/TransactionSuccess";
 import PaymentSuccess from "./pages/PaymentSuccess";
 
-
 import ProcessingPage from "./components/ProcessingPage";
 import PaymentModal from "./components/PaymentModal";
 
@@ -16,26 +15,24 @@ import SearchPage from "./pages/SearchPage";
 import SearchpageSummary from "./pages/SearchpageSummary";
 import { useState } from "react";
 import ProfileSignup from "./pages/ProfileSignup";
-
-
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [selectedMedicines, setSelectedMedicines] = useState([]);
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
 
       <Route path="/findmeds" element={<FindMeds />} />
-      <Route path="/transactionsuccess" element={<TransactionSuccess/>}/>
-      <Route path="/paymentsuccess" element={<PaymentSuccess/>}/>
+      <Route path="/transaction-success" element={<TransactionSuccess />} />
+      <Route path="/payment-success" element={<PaymentSuccess />} />
 
+      <Route path="/processing" element={<ProcessingPage />} />
 
-      <Route path = "/processing" element = {<ProcessingPage/>}/>
-
-      <Route path = "/paymentmodal" element = {<PaymentModal/>}/>
-
+      <Route path="/payment-modal" element={<PaymentModal />} />
+      <Route path="/dashboard" element={<Dashboard />} />
 
       <Route
         path="/findmeds"
@@ -47,10 +44,10 @@ function App() {
         }
       />
       <Route path="/search-summary" element={<SearchpageSummary />} />
-      <Route path="/complete-profile" element={<ProfileSignup />} 
 
+      <Route path="/complete-profile" element={<ProfileSignup />} />
     </Routes>
-  ); 
+  );
 }
 
 export default App;
